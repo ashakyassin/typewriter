39874a23003e24fb011243377e8142f1466b7e8b
ashakyassin <asha.kyassin@gmail.com>
2023-02-15 00:06:46 +0000

    Created Typewriter function

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+const increment = 50;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, delay);
+  delay += increment;
+};
+
+let delay2 = increment * sentence.length;
+setTimeout(() => {
+  console.log("\n");
+}, delay2);
